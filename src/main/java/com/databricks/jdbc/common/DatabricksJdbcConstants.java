471ce414a00cf5bc1268c939c4ea7a891f379c4f
Madhavendra Rathore <madhavendra.rathore@databricks.com>
2025-08-26 19:08:10 +0530

    Added unit test for isValid() and removed ENABLE_SQL_VALIDATION_FOR_IS_VALID from client info properties

--- src/main/java/com/databricks/jdbc/common/DatabricksJdbcConstants.java
+++ src/main/java/com/databricks/jdbc/common/DatabricksJdbcConstants.java
@@ -90,8 +90,7 @@ public final class DatabricksJdbcConstants {
           ENABLE_VOLUME_OPERATIONS,
           ALLOWED_STAGING_INGESTION_PATHS,
           DatabricksJdbcUrlParams.AUTH_ACCESS_TOKEN.getParamName(),
-          DatabricksJdbcUrlParams.APPLICATION_NAME.getParamName(),
-          DatabricksJdbcUrlParams.ENABLE_SQL_VALIDATION_FOR_IS_VALID.getParamName());
+          DatabricksJdbcUrlParams.APPLICATION_NAME.getParamName());
   public static final Map<String, String> JSON_HTTP_HEADERS =
       Map.of(
           "Accept", "application/json",
